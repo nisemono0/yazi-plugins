d99d8f1b236f9823e92edbe6a7debe32c2f1b942
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-06-01 21:05:50 +0300

    Add plugin

---
+++ open-with-cmd.yazi/README.md
@@ -0,0 +1,19 @@
+# open-with-cmd.yazi
+Open file(s) with command
+
+## Installation
+```sh
+ya pkg add nisemono0/yazi-plugins:open-with-cmd
+```
+
+## Usage
+Add this to your `keymap.toml`:
+```toml
+[mgr]
+prepend_keymap = [
+    { on = "<A-o>", run = "plugin open-with-cmd", desc = "Open with command in the terminal" },
+    { on = "<A-O>", run = "plugin open-with-cmd block", desc = "Open with command in the terminal (block)" },
+]
+```
+
+
---
+++ open-with-cmd.yazi/main.lua
@@ -0,0 +1,21 @@
+-- Edited from: https://github.com/Ape/open-with-cmd.yazi
+
+return {
+    entry = function(_, job)
+        local block = job.args[1] and job.args[1] == "block"
+
+        local value, event = ya.input({
+            title = block and "Open with (block):" or "Open with:",
+            position = { "hovered", y = 1, w = 50 },
+        })
+
+        if event == 1 then
+            ya.emit("shell", {
+                value .. ' "$@"',
+                block = block,
+                orphan = true,
+                confirm = true,
+            })
+        end
+    end,
+}
