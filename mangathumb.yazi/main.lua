178fe33c92657f2ff23276c2af8206b65316d7a4
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-09-24 22:16:02 +0300

    Update plugin * Moved archive list and extract to different functions * Run normal archive seek on seek

--- mangathumb.yazi/main.lua
+++ mangathumb.yazi/main.lua
@@ -27,49 +27,89 @@ function M:preload(job)
         return true
     end
 
-    local arch_command = Command("7z")
-        :arg({ "-ba", "l", tostring(job.file.url) })
+    local filelist, err = self.get_arch_filelist(job.file.url)
+    if not filelist then
+        return false, Err("Failed to get archive filelist", err)
+    end
+
+    job.skip = job.skip >= #filelist and (#filelist - 1) or job.skip
+
+    if job.skip == -1 then
+        return false, Err("No images in archive: " .. tostring(job.file.url))
+    end
+
+    local image_filename = filelist[job.skip + 1]
+    local image_data, err = self.get_arch_image(job.file.url, image_filename)
+    if not image_data then
+        return false, Err("Failed to get image data", err)
+    end
+
+    write_img, write_img_err = fs.write(cache, image_data)
+    if not write_img then
+        return false, Err("Failed to write image cache", write_img_err)
+    end
+    return true
+
+    -- This pre-caches the saved full res image to one based on user max_{width,height}
+    -- Returning this instead of true basically does 2 writes on disk
+    -- Performance hit is not noticeable
+    -- return ya.image_precache(cache, cache)
+end
+
+function M:seek(job) require("archive"):seek(job) end
+
+function M.get_arch_filelist(url)
+    local arch_child, err = Command("7z")
+        :arg({ "-ba", "l", tostring(url) })
         :stdout(Command.PIPED)
         :stderr(Command.PIPED)
         :spawn()
+    if not arch_child then
+        return false, Err("Failed to spawn 7z", err)
+    end
 
-    local awk_command = Command("awk")
+    local arch_stdout = arch_child:take_stdout()
+    if not arch_stdout then
+        return false, Err("Failed to list archive with 7z")
+    end
+
+    local awk_child, err = Command("awk")
         :arg({
             [[length($0) > 53 && tolower(substr($0, 54)) ~ /\.(jpg|jpeg|png|gif)$/ { print substr($0, 54) }]]
         })
-        :stdin(arch_command:take_stdout())
+        :stdin(arch_stdout)
         :stdout(Command.PIPED)
         :stderr(Command.PIPED)
         :spawn()
+    if not awk_child then
+        return false, Err("Failed to spawn awk", err)
+    end
 
-    local awk_output, event = awk_command:wait_with_output()
-    arch_command:start_kill()
+    local awk_output, err = awk_child:wait_with_output()
+    if not awk_output then
+        return false, Err("Can't read awk output", err)
+    end
 
     local filenames = {}
     for filename in awk_output.stdout:gmatch("[^\n]+") do
         table.insert(filenames, filename)
     end
     table.sort(filenames)
+    return filenames
+end
 
-    job.skip = job.skip >= #filenames and (#filenames - 1) or job.skip
-
-    if job.skip == -1 then
-        return false, Err("No images in archive")
-    end
-
-    local extract_output = Command("7z")
-        :arg({ "-so", "e", tostring(job.file.url), filenames[job.skip + 1] })
+function M.get_arch_image(url, filename)
+    local extract_output, err = Command("7z")
+        :arg({ "-so", "e", tostring(url), tostring(filename) })
         :stdout(Command.PIPED)
         :stderr(Command.PIPED)
         :output()
 
-    write_out, write_err = fs.write(cache, extract_output.stdout)
-    if not write_out then
-        return false, Err("Failes to write cache: %s", write_err)
+    if not extract_output then
+        return false, Err("Failed to extract image with 7z", err)
     end
-    return true
-end
 
-function M:seek() end
+    return extract_output.stdout
+end
 
 return M
